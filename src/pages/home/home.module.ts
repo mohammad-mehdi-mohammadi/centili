25b436fcb1b231104cf380e508b156d6045504d1
linz <phoneticem@gmail.com>
2022-05-04 10:04:56 +0430

    index page

--- src/pages/home/home.module.ts
+++ src/pages/home/home.module.ts
@@ -2,15 +2,19 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {HomeComponent} from './home.component';
 import {HomeRoutingModule} from "./home-routing.module";
+import { FeaturedLogosComponent } from './components/featured-logos/featured-logos.component';
+import {SwiperModule} from "swiper/angular";
 
 
 @NgModule({
     declarations: [
-        HomeComponent
+        HomeComponent,
+        FeaturedLogosComponent
     ],
     imports: [
         CommonModule,
-        HomeRoutingModule
+        HomeRoutingModule,
+        SwiperModule
     ]
 })
 export class HomeModule {
