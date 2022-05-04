327a36d30c84d48bbf03f2b338c788c5fb6025fc
linz <phoneticem@gmail.com>
2022-05-05 03:25:19 +0430

    index page- design

--- src/pages/home/home.module.ts
+++ src/pages/home/home.module.ts
@@ -6,12 +6,18 @@ import { FeaturedLogosComponent } from './components/featured-logos/featured-log
 import {SwiperModule} from "swiper/angular";
 import {HomeLayoutModule} from "../../layouts/home-layout/home-layout.module";
 import {SocialNetworkModule} from "../../shared/components/social-network/social-network.module";
+import { GrowthPartnerComponent } from './components/growth-partner/growth-partner.component';
+import { ImpactIndustriesComponent } from './components/impact-industries/impact-industries.component';
+import { EmpoweringBusinessesComponent } from './components/empowering-businesses/empowering-businesses.component';
 
 
 @NgModule({
     declarations: [
         HomeComponent,
-        FeaturedLogosComponent
+        FeaturedLogosComponent,
+        GrowthPartnerComponent,
+        ImpactIndustriesComponent,
+        EmpoweringBusinessesComponent
     ],
     imports: [
         CommonModule,
