e9af59c98baa28d5d42a09e43bcd21dd4b99fc99
linz <phoneticem@gmail.com>
2022-05-06 20:45:25 +0430

    nav responsive

--- src/layouts/home-layout/components/header/components/sub-menu/sub-menu.component.ts
+++ src/layouts/home-layout/components/header/components/sub-menu/sub-menu.component.ts
@@ -1,25 +1,37 @@
-import {Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
 import {NavEnum} from "../../../../../../shared/enums/nav-enum";
 
 @Component({
-  selector: 'app-sub-menu',
-  templateUrl: './sub-menu.component.html',
-  styleUrls: ['./sub-menu.component.scss'],
+    selector: 'app-sub-menu',
+    templateUrl: './sub-menu.component.html',
+    styleUrls: ['./sub-menu.component.scss'],
 })
 export class SubMenuComponent implements OnInit {
-  @Input() navName: NavEnum = this.getNavEnum.NONE;
-  @Output() hideSubMenu = new EventEmitter<NavEnum>();
-  get getNavEnum() {
-    return NavEnum;
-  }
-  constructor() { }
+    @Input() navName: NavEnum = this.getNavEnum.NONE;
+    @Output() hideSubMenu = new EventEmitter<NavEnum>();
+    public responsiveTranslationAction = this.getNavEnum.NONE;
 
-  ngOnInit(): void {
-  }
+    get getNavEnum() {
+        return NavEnum;
+    }
 
-  public onHideSubMenuChange(): void {
-    this.navName = this.getNavEnum.NONE;
-    this.hideSubMenu.emit(this.getNavEnum.NONE);
-  }
+    constructor() {
+    }
+
+    ngOnInit(): void {
+    }
+
+    public onHideSubMenuChange(): void {
+        this.navName = this.getNavEnum.NONE;
+        this.hideSubMenu.emit(this.getNavEnum.NONE);
+        this.responsiveTranslationAction = this.getNavEnum.NONE;
+    }
+
+    public responsiveSubMenuHandler(navName: NavEnum): void {
+        this.responsiveTranslationAction = navName;
+    }
+    public back(): void {
+        this.responsiveTranslationAction = this.getNavEnum.NONE;
+    }
 
 }
